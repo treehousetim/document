1bae70b789686b11992df80440a41badfd06c826
Tim Gallagher <treehousetim@gmail.com>
2020-07-01 10:20:51 -0500

    Create Exception.php

---
+++ src/Exception.php
@@ -0,0 +1,10 @@
+<?php namespace application\libraries\documents;
+
+class Exception extends \LogicException
+{
+	const undefined = -1;
+	const missingData = 1;
+	const wrongType = 2;
+	const callOneVar = 3;
+	const noSuchProperty = 4;
+}
